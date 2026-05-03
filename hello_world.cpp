bf356618732412b5d42553bae20bf4f789cec362
MarsiSomeone <sirlovaolga@gmail.com>
2026-05-03 12:49:00 +0300

    upgrade hello_world patch1 second time

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,8 @@
-#include <iostream>
-#include <string>
+//main function
 int main(){
-
-std::string n;
-std::cout<<"Enter name: ";
-std::cin >> n;
-std::cout<<std::endl<<"Hello world from "<<n<<std::endl;
- return 0;}
+//world execute.(me)
+std::string n;//variabre
+std::cout<<"Enter name: ";//output comment
+std::cin >> n;//input function
+std::cout<<std::endl<<"Hello world from "<<n<<std::endl;//final output
+return 0;}
