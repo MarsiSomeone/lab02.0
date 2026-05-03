7a82c3f9a4f08a853fd56496f4eb0a9c6d5eff4e
MarsiSomeone <sirlovaolga@gmail.com>
2026-05-03 23:22:43 +0300

    upgrade hello_world

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main()
+int
+main()
 {
-string n;
-cout<<"Enter ypur name: "<<endl;
-cin>>n;
-cout<<"Hello world from "<<n <<endl;
-return 0;
+  string n;
+  cout << "Enter ypur name: " << endl;
+  cin >> n;
+  cout << "Hello world from " << n << endl;
+  return 0;
 }
